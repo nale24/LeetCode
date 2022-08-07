36105e78a055982f574c4c63954aa1334ca5813a
Na Le <43465771+nale24@users.noreply.github.com>
2022-08-07 05:16:17 -0400

    Time: 16 ms (84.32%), Space: 39 MB (94.43%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,7 +3,6 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-
         int left = 1, right = n, mid = 0;
         while (left < right) {
             mid = left + (right - left)/2;
@@ -15,5 +14,6 @@ public class Solution extends VersionControl {
             }
         } //while
         return left;
+        
     }
 }
