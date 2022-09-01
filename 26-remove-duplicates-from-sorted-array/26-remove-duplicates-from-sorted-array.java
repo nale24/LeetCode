a3c3a116bc5681b4e4d4775284e54d34c54412fd
Na Le <43465771+nale24@users.noreply.github.com>
2022-08-31 22:49:49 -0400

    Time: 1 ms (100.00%), Space: 48.3 MB (15.92%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 1) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[count]) {
+                count++;
+                nums[count] = nums[i];
+            }
+        }
+        return (count + 1);
+    }
+}
