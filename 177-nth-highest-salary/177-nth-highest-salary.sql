bca29e688199059cb1fafdde5552b12648cd9412
Na Le <43465771+nale24@users.noreply.github.com>
2022-09-17 22:51:11 -0400

    Time: 544 ms (35.09%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -4,6 +4,6 @@ set n = n - 1;
   RETURN (
       # Write your MySQL query statement below.
       ifnull(
-          (select distinct Salary as getNthHighestSalary from Employee order by Salary desc limit 1 offset n), null)
+          (select distinct Salary as getNthHighestSalary from Employee order by Salary desc limit n, 1), null)
 );
 END
